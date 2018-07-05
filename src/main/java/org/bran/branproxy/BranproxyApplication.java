2899d90f5ea21ba3f53fdae123a649c40a40b2c4
BranSummer <bransummer524@gmail.com>
2018-07-05 21:37:11 +0800

    first commit
    想要完成一个代理池

---
+++ src/main/java/org/bran/branproxy/BranproxyApplication.java
@@ -0,0 +1,12 @@
+package org.bran.branproxy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BranproxyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BranproxyApplication.class, args);
+    }
+}
